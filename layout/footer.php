805a299ac31f4b403981770782e34d49c605d552
Dima <15dimon03@gmail.com>
2020-03-10 00:16:35 +0200

    edit header and footer
    ссилки на соц мережі, додаткова інформація , контактна інфа та карта гугл

--- layout/footer.php
+++ layout/footer.php
@@ -1,104 +1,60 @@
 <div class="bg-primary" data-aos="fade">
-  <div class="container">
-    <div class="row">
-      <a href="#" class="col-2 text-center py-4 social-icon d-block"><span class="icon-facebook text-white"></span></a>
-      <a href="#" class="col-2 text-center py-4 social-icon d-block"><span class="icon-twitter text-white"></span></a>
-      <a href="#" class="col-2 text-center py-4 social-icon d-block"><span class="icon-instagram text-white"></span></a>
-      <a href="#" class="col-2 text-center py-4 social-icon d-block"><span class="icon-linkedin text-white"></span></a>
-      <a href="#" class="col-2 text-center py-4 social-icon d-block"><span class="icon-pinterest text-white"></span></a>
-      <a href="#" class="col-2 text-center py-4 social-icon d-block"><span class="icon-youtube text-white"></span></a>
+    <div class="container">
+        <div class="row">
+            <a href="#" class="col-2 text-center py-4 social-icon d-block"><span class=" text-white"></span></a>
+            <a href="#" class="col-2 text-center py-4 social-icon d-block"><span class=" text-white"></span></a>
+            <a target="_blank" href="https://www.facebook.com/people/%D0%A7%D0%B0%D0%BF%D0%BB%D1%96%D0%BD-%D0%94%D0%BE%D0%BB%D0%B8%D0%BD%D0%B0/100018903855555" class="col-2 text-center py-4 social-icon d-block"><span class="icon-facebook text-white"></span></a>
+            <a target="_blank" href="https://www.instagram.com/kinoteatrchaplin9996/?hl=uk" class="col-2 text-center py-4 social-icon d-block"><span class="icon-instagram text-white"></span></a>
+        </div>
     </div>
-  </div>
 </div>
 
-<footer class="site-footer">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-4">
-        <div class="mb-5">
-          <h3 class="footer-heading mb-4">About Apart</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe pariatur reprehenderit vero atque, consequatur id ratione, et non dignissimos culpa? Ut veritatis, quos illum totam quis blanditiis, minima minus odio!</p>
-        </div>
-        <div class="mb-5">
-          <h3 class="footer-heading mb-4">Subscribe</h3>
-          <form action="#" method="post" class="site-block-subscribe">
-            <div class="input-group mb-3">
-              <input type="text" class="form-control border-secondary bg-transparent" placeholder="Enter your email"
-                aria-label="Enter Email" aria-describedby="button-addon2">
-              <div class="input-group-append">
-                <button class="btn btn-primary rounded-top-right-0" type="button" id="button-addon2">Subscribe</button>
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
-      <div class="col-lg-4 mb-5 mb-lg-0">
-        <div class="row mb-5">
-          <div class="col-md-12">
-            <h3 class="footer-heading mb-4">Navigations</h3>
-          </div>
-          <div class="col-md-6 col-lg-6">
-            <ul class="list-unstyled">
-              <li><a href="#">Home</a></li>
-              <li><a href="#">Apartments</a></li>
-              <li><a href="#">News</a></li>
-              <li><a href="#">Featured Apartment</a></li>
-            </ul>
-          </div>
-          <div class="col-md-6 col-lg-6">
-            <ul class="list-unstyled">
-              <li><a href="#">About Us</a></li>
-              <li><a href="#">Privacy Policy</a></li>
-              <li><a href="#">Contact Us</a></li>
-              <li><a href="#">Membership</a></li>
-            </ul>
-          </div>
-        </div>
-
+<footer id="contact" class="site-footer">
+    <div class="container">
         <div class="row">
-          <div class="col-md-12">
-            <h3 class="footer-heading mb-4">Follow Us</h3>
+            <div class="col-lg-5">
+                <div class="mb-5">
+                    <h3 class="footer-heading mb-4">Про кінотеатр "Чаплін"</h3>
+                    <p>Міський кінотеатр знаходиться за адресою
+                        вул. Грушевського, 7, м. Долина, Івано-Франківська обл., 77500.
+                        Відкритий у 2014 році. У кінозалі є 54 комфортні місця для перегляду фільмів. Також при кінотеатрі
+                        є кілька столиків та можливість
+                        придбати напої та різні снеки до фільму.</p>
+                    </div>
 
-            <div>
-              <a href="#" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
-              <a href="#" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
-              <a href="#" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
-              <a href="#" class="pl-3 pr-3"><span class="icon-linkedin"></span></a>
+                    <div class="mb-5">
+                        <div class="col-lg-6">
+                            <div class="mb-5">
+                                <h3 class="footer-heading mb-4">Телефончик</h3>
+                                <p>Телефон для бронювання квитків 0953269996 (10:00-22:00)</p>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- <div class="mb-5">
+                    <h3 class="footer-heading mb-4">Subscribe</h3>
+                    <form action="#" method="post" class="site-block-subscribe">
+                    <div class="input-group mb-3">
+                    <input type="text" class="form-control border-secondary bg-transparent" placeholder="Enter your email"
+                    aria-label="Enter Email" aria-describedby="button-addon2">
+                    <div class="input-group-append">
+                    <button class="btn btn-primary rounded-top-right-0" type="button" id="button-addon2">Subscribe</button>
+                </div>
             </div>
-          </div>
-        </div>
+        </form>
+    </div> -->
+</div>
 
-      </div>
-
-      <div class="col-lg-4 mb-5 mb-lg-0">
-        <div class="mb-5">
-          <h3 class="footer-heading mb-4">Watch Live Streaming</h3>
-
-          <div class="block-16">
+<div class="col-lg-7 mb-2 mb-lg-0">
+    <div class="mb-5">
+        <h3 class="footer-heading mb-4">Нас легко знайти</h3>
+        <div class="block-16">
             <figure>
-              <img src="images/img_1.jpg" alt="Image placeholder" class="img-fluid rounded">
-              <a href="https://vimeo.com/channels/staffpicks/93951774" class="play-button popup-vimeo"><span class="icon-play"></span></a>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d755.9573630950568!2d23.97694362652029!3d48.97613599642037!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473a0f5505315c49%3A0x374636ee36ec59ce!2z0JrRltC90L7RgtC10LDRgtGAINCn0LDQv9C70ZbQvQ!5e0!3m2!1sru!2sua!4v1583791182807!5m2!1sru!2sua" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
             </figure>
-          </div>
-
         </div>
-
-
-
-      </div>
-
     </div>
-    <div class="row pt-5 mt-5 text-center">
-      <div class="col-md-12">
-        <p>
-        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-        Copyright &copy;<script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart text-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" >Colorlib</a>
-        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-        </p>
-      </div>
-
-    </div>
-  </div>
+</div>
+</div>
 </footer>
 
 </div>
--- layout/header.php
+++ layout/header.php
@@ -36,41 +36,20 @@
         <div class="container py-1">
           <div class="row align-items-center">
             <div class="col-8 col-md-8 col-lg-4">
-              <h1 class="mb-0"><a href="index.html" class="text-white h2 mb-0"><strong>Apart<span class="text-primary">.</span></strong></a></h1>
+              <h1 class="mb-0"><a href="index.php" class="text-white h2 mb-0"><strong>Кінотеатр "Долина"</strong></a></h1>
             </div>
             <div class="col-4 col-md-4 col-lg-8">
               <nav class="site-navigation text-right text-md-right" role="navigation">
 
                 <div class="d-inline-block d-lg-none ml-md-0 mr-auto py-3"><a href="#" class="site-menu-toggle js-menu-toggle text-white"><span class="icon-menu h3"></span></a></div>
 
-                <ul class="site-menu js-clone-nav d-none d-lg-block">
-                  <li class="active">
-                    <a href="index.html">Home</a>
-                  </li>
-                  <li><a href="about.html">About</a></li>
-                  <li class="has-children">
-                    <a href="apartments.html">Apartments</a>
-                    <ul class="dropdown arrow-top">
-                      <li><a href="#">Apartments</a></li>
-                      <li><a href="#">Rooms</a></li>
-                      <li><a href="#">Suites</a></li>
-                      <li class="has-children">
-                        <a href="#">Sub Menu</a>
-                        <ul class="dropdown">
-                          <li><a href="#">Menu One</a></li>
-                          <li><a href="#">Menu Two</a></li>
-                          <li><a href="#">Menu Three</a></li>
-                        </ul>
-                      </li>
-                    </ul>
-                  </li>
-                  <li><a href="news.html">News</a></li>
-                  <li><a href="contact.html">Contact</a></li>
+                <ul style="font-size:22px;" class="site-menu js-clone-nav d-none d-lg-block">
+                  <li class="active"><a href="?action=session">Сеанси</a></li>
+                  <li class="active"><a href="?action=films">Фільми</a></li>
+                  <li class="active"><a href="#contact">Контакти</a></li>
                 </ul>
               </nav>
             </div>
-
-
           </div>
         </div>
       </div>
@@ -89,9 +68,9 @@
       <div class="container">
         <div class="row align-items-center justify-content-center">
           <div class="col-md-8 text-center" data-aos="fade-up" data-aos-delay="400">
-            <h1 class="mb-4">Excellent Space For Your Next Home</h1>
-            <p class="mb-5">1105 Madison Plaza Suite 120 Chesapeake, CA, California</p>
-            <p><a href="#" class="btn btn-primary px-5 py-3">Take a Tour</a></p>
+            <h1 class="mb-4">Приємне місце відпочику для приємних людей</h1>
+            <p class="mb-5">вулиця Грушевського, 7, Долина, Івано-Франківська область, 77500</p>
+            <p><a href="?action=session" class="btn btn-primary px-5 py-3">Перейти до сеансів</a></p>
           </div>
         </div>
       </div>
